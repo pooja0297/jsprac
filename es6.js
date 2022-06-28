aa2f33209dd381297d5ba484ec3f61946f8ece0c
Pooja Gujja <poojagujja1997@gmail.com>
2022-06-28 23:17:28 +0530

    for loops js

---
+++ es6.js
@@ -0,0 +1,32 @@
+const actors = [
+    {
+        name: "actor1",
+        payment: 100,
+    },
+    {
+        name: "actor2",
+        payment: 200,
+    },
+    {
+        name: "actor3",
+        payment: 300,
+    }
+]
+// let sum = 0;
+let array = [];
+
+// console.log(sum);
+for (let i = 0; i < actors.length; i++) {
+
+    actorsnameget =  actors[i].name;
+    array.push(actorsnameget);
+   
+}
+console.log(array);
+// console.log(actors[2].name);
+
+
+
+
+
+// let sum = actors[0].payment +  actors[1].payment +  actors[2].payment;
